00ea348fc0cfba99926e7c12cc547d473d0dbf37
Sam Burch <sam.burch85@gmail.com>
2022-11-21 18:22:47 +0000

    Age validations factors in day

--- src/DateOfBirth.ts
+++ src/DateOfBirth.ts
@@ -6,17 +6,25 @@ interface ValueObject {
 export class DateOfBirth implements ValueObject {
   age?: number;
   constructor(public date: string) {
-    this.age = DateOfBirth.getAge(date);
     this.date = DateOfBirth.isValidDate(date);
+    this.age = this.getAge(date);
   }
 
-  static getAge(value: string): number {
-    return new Date().getFullYear() - new Date(value).getFullYear();
-  }
-
+  // Validator
   static isValidDate(date: string): any {
-    const age = this.getAge(date);
-    if (age < 13) throw Error("You must be 13 years old or more");
-    return new Date(date).toISOString();
+    const thirteenYears = new Date(
+      new Date().setFullYear(new Date().getFullYear() - 13)
+    );
+    const input = new Date(date);
+    if (input >= thirteenYears) {
+      throw Error("You must be more than 13 years old");
+    } else {
+      return new Date(date).toISOString();
+    }
+  }
+
+  // Factory method
+  private getAge(value: string) {
+    return new Date().getFullYear() - new Date(value).getFullYear();
   }
 }
